78b4892b98dbea111cb16ebfddba53328376ef60
asprin107 <asprin107@gmail.com>
2020-10-20 16:40:59 +0900

    init

---
+++ src/main/java/com/saltlux/srttojson/SrttojsonApplication.java
@@ -0,0 +1,13 @@
+package com.saltlux.srttojson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrttojsonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SrttojsonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/saltlux/srttojson/SrttojsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saltlux.srttojson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrttojsonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
